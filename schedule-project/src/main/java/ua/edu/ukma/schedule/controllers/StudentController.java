500400d23aeabffd9b338ea79a3309416761432a
Taisiia Shutiak <taisiia.shutiak@ukma.edu.ua>
2021-11-02 14:51:45 +0200

    add swagger

--- schedule-project/src/main/java/ua/edu/ukma/schedule/controllers/StudentController.java
+++ schedule-project/src/main/java/ua/edu/ukma/schedule/controllers/StudentController.java
@@ -1,5 +1,8 @@
 package ua.edu.ukma.schedule.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
@@ -14,28 +17,45 @@ import javax.validation.Valid;
 @RequestMapping("/api/student")
 @RequiredArgsConstructor
 @Log4j2
+@Tag(name="StudentController", description="Processing operations with students")
 public class StudentController {
 
     private final StudentService studentService;
 
+    @Operation(
+            summary = "Adding new student",
+            description = "Let add a new student"
+    )
     @PostMapping(value = "/")
     public CustomResponse<User> create(@RequestBody @Valid Student user) {
         return CustomResponse.of(studentService.save(user));
     }
 
+    @Operation(
+            summary = "Getting a student",
+            description = "Get a student with given id"
+    )
     @GetMapping(value = "/{id}")
-    public CustomResponse<Student> read(@PathVariable(value = "id") Long id) {
+    public CustomResponse<Student> read(@PathVariable(value = "id") @Parameter(description = "Student id")  Long id) {
         return CustomResponse.of(studentService.getById(id));
     }
 
+    @Operation(
+            summary = "Updating a student",
+            description = "Let update a student"
+    )
     @PutMapping(value = "/")
     public CustomResponse<Boolean> update(@RequestBody @Valid Student user) {
         studentService.save(user);
         return CustomResponse.of(true);
     }
 
+    @Operation(
+            summary = "Deleting a student",
+            description = "Let delete a student"
+    )
     @DeleteMapping(value = "/{id}")
-    public CustomResponse<Boolean> delete(@PathVariable(value = "id") Long id) {
+    public CustomResponse<Boolean> delete(@PathVariable(value = "id") @Parameter(description = "Student id")  Long id) {
         studentService.delete(id);
         return CustomResponse.of(true);
     }
