101cfa95f8aeedc0287f0b52b8e6cc56866f5460
Taisiia Shutiak <taisiia.shutiak@ukma.edu.ua>
2021-11-24 17:34:40 +0200

    add resourse file

---
+++ schedule-project/src/main/java/ua/edu/ukma/schedule/config/LocaleConfig.java
@@ -0,0 +1,17 @@
+package ua.edu.ukma.schedule.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class LocaleConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("messages");
+        resourceBundleMessageSource.setDefaultEncoding("UTF-8");
+        return resourceBundleMessageSource;
+    }
+}
