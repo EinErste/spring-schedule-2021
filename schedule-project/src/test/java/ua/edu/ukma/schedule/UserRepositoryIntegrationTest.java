d7fb35aa4bd78aded7ddbd6c37ffb99a711fd6b5
YanaKrukovska <jana.krua@gmail.com>
2021-12-01 21:01:33 +0200

    - Migrate UserRepositoryIntegrationTest to JUnit 5

--- schedule-project/src/test/java/ua/edu/ukma/schedule/UserRepositoryIntegrationTest.java
+++ schedule-project/src/test/java/ua/edu/ukma/schedule/UserRepositoryIntegrationTest.java
@@ -1,6 +1,6 @@
 package ua.edu.ukma.schedule;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,10 +12,12 @@ import ua.edu.ukma.schedule.repositories.UserRepository;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class UserRepositoryIntegrationTest {
+class UserRepositoryIntegrationTest {
 
     @Autowired
     private TestEntityManager entityManager;
@@ -23,11 +25,12 @@ public class UserRepositoryIntegrationTest {
     private UserRepository userRepository;
 
     @Test
-    public void whenFindByEmailAndUserExistsThenReturnUser() {
-        User testUser = User.builder().email("taisiia@example.com").name("Taisiia"). surname("Shutiak").password("pass").build();
+    void whenFindByEmailAndUserExistsThenReturnUser() {
+        User testUser = User.builder().email("taisiia@example.com").name("Taisiia").surname("Shutiak").password("pass").build();
         entityManager.persist(testUser);
         entityManager.flush();
         Optional<User> foundUser = userRepository.findUserByEmail("taisiia@example.com");
+        assertTrue(foundUser.isPresent());
         assertThat(foundUser.get().getName())
                 .isEqualTo(testUser.getName());
         assertThat(foundUser.get().getSurname())
@@ -37,10 +40,10 @@ public class UserRepositoryIntegrationTest {
         assertThat(foundUser.get().getPassword())
                 .isEqualTo(testUser.getPassword());
     }
+
     @Test
-    public void whenFindByEmailAndUserNotExistsThenReturnOptionalWithNull() {
+    void whenFindByEmailAndUserNotExistsThenReturnOptionalWithNull() {
         Optional<User> foundUser = userRepository.findUserByEmail("taisiia@example.com");
-        assertThat(foundUser.isPresent())
-                .isEqualTo(false);
+        assertFalse(foundUser.isPresent());
     }
 }
