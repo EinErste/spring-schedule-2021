23afa1b5a98b4a7d3e33604e58e28b288b4b8211
Taisiia Shutiak <taisiia.shutiak@ukma.edu.ua>
2021-12-10 01:39:05 +0200

    delete user

--- schedule-project/src/main/java/ua/edu/ukma/schedule/controllers/UserController.java
+++ schedule-project/src/main/java/ua/edu/ukma/schedule/controllers/UserController.java
@@ -1,8 +1,12 @@
 package ua.edu.ukma.schedule.controllers;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +14,10 @@ import ua.edu.ukma.schedule.model.Staff;
 import ua.edu.ukma.schedule.model.Student;
 import ua.edu.ukma.schedule.model.User;
 import ua.edu.ukma.schedule.services.UserService;
+import ua.edu.ukma.schedule.util.CustomResponse;
+import ua.edu.ukma.schedule.util.CustomResponseError;
+
+import java.util.Optional;
 
 import static ua.edu.ukma.schedule.util.ConstantsUtil.USER_LABEL;
 import static ua.edu.ukma.schedule.util.ConstantsUtil.USER_LIST_LABEL;
@@ -27,8 +35,8 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public String getUserInfo(Model model, @PathVariable String id){
-        User user = userService.getById(Long.valueOf(id));
+    public String getUserInfo(Model model, @PathVariable Long id){
+        User user = userService.getById(id);
         boolean isStudent = user instanceof Student;
         boolean isMethodist = user instanceof Staff;
         model.addAttribute("isStudent", isStudent);
@@ -38,6 +46,8 @@ public class UserController {
         model.addAttribute("user", user);
         return USER_LABEL;
     }
+
+
 
 
 }
